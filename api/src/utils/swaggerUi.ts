c1daf2de087a6a5df7d96e94bd2c9d9c6fa0c212
Erold Ramos <dev.eroldramos@gmail.com>
2025-08-13 09:10:26 +0800

    push

--- api/src/utils/swaggerUi.ts
+++ api/src/utils/swaggerUi.ts
@@ -35,5 +35,14 @@ const options: swaggerJSDoc.Options = {
 const swaggerSpec = swaggerJSDoc(options);
 
 export function setupSwagger(app: Express): void {
-  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use(
+    "/api/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, {
+      customCss:
+        ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+      customCssUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.1.0/swagger-ui.min.css",
+    }),
+  );
 }
